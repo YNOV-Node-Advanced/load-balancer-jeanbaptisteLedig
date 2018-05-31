4b95219e27f9829830d2cd02747a319460bbbf93
Ledig <jeanbaptiste.ledig@ynov.com>
2018-05-31 17:10:07 +0200

    Loadbalancer

---
+++ loadbalancer.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const request = require('request');
+const servers = ['http://localhost:3000', 'http://localhost:3001' ];
+let cur = 0;   
+
+const handler = (req, res) => {
+    const _req = request({ url: servers[cur] + req.url }).on('error', error => {
+        res.status(500).send(error.message);
+        });
+        req.pipe(_req).pipe(res);
+        cur = (cur + 1) % servers.length;
+        };
+        const server = express().get('*', handler).post('*', handler);
+        
+        server.listen(8080);
