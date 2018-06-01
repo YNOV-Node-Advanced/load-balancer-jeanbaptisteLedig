c5b78dcea92fd0cefac900e0ba267ab9c55f31c2
Ledig <jeanbaptiste.ledig@ynov.com>
2018-06-01 09:49:26 +0200

    je commit tout

--- loadbalancer.js
+++ loadbalancer.js
@@ -7,9 +7,9 @@ const handler = (req, res) => {
     const _req = request({ url: servers[cur] + req.url }).on('error', error => {
         res.status(500).send(error.message);
         });
-        req.pipe(_req).pipe(res);
-        cur = (cur + 1) % servers.length;
+            req.pipe(_req).pipe(res);
+            cur = (cur + 1) % servers.length;
         };
-        const server = express().get('*', handler).post('*', handler);
-        
-        server.listen(8080);
+const server = express().get('*', handler).post('*', handler);
+
+server.listen(8080);
